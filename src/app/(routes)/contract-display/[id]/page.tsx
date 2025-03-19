54531160942a4e5ebbb7360a587981dac0e652cb
KalweCaramalac <kalwe.dev@gmail.com>
2025-03-19 13:41:38 -0400

    remove shit

--- src/app/(routes)/contract-display/[id]/page.tsx
+++ src/app/(routes)/contract-display/[id]/page.tsx
@@ -2,18 +2,10 @@ import ContractForm from '@/app/components/display/contract-form'
 import { EmployeeService } from '@/app/services/employeeService'
 import { Employee } from '@/app/types/employee'
 import { Navigation } from 'lucide-react'
-import { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  // needs to be on server
-  title: 'Presentation'
-}
-
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id
-  console.log(id)
+export default async function Page({ params }: { params: Promise<{ id: number }> }) {
+  const { id } = await params
   const employeeData: Employee = await EmployeeService.getEmployeeById(Number(id))
-  console.log(employeeData)
 
   return (
     <div className='mx-auto mt-10'>
