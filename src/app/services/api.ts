b13882dd086ebf704a30601acdc9d23151853df0
KalweCaramalac <kalwe.dev@gmail.com>
2025-03-19 07:49:00 -0400

    change url api

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const host = process.env.NEXT_PUBLIC_HOST || 'localhost'
-const port = process.env.NEXT_PUBLIC_PORT || '3001'
+const host = process.env.NEXT_PUBLIC_HOST || '3.95.149.55'
+const port = process.env.NEXT_PUBLIC_PORT || '8080'
 const api_prefix = 'api/v1'
 
 const api = axios.create({
